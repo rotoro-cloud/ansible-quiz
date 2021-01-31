c2a9df7d98ce8335a8f5fca45ab864cd94433c87
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2021-01-31 12:43:38 +0300

    Update ansible_playbook_lab.js

--- static/ansible_playbook_lab.js
+++ static/ansible_playbook_lab.js
@@ -145,5 +145,55 @@ boston_nodes
 dallas_nodes`
           }
         ]
+    },
+    {
+        "title": "Ansible Playbooks",
+        "question": `Мы получили задание на перезапуск нескольких серверов в определенной последовательности.
+Последовательность и команды для исполнения приведены ниже. Обрати внимание, что эти команды нужно запустить только на подходящих для этого серверах.
+Изучи inventory-файл и обнови `playbook`, чтобы последовательность была правильной.
+
+> ИНФО: Используй приведенное ниже описание для \`plays\` и \`tasks\`.
+
+1. \`Stop\` the \`web\` services on web server nodes - \`service httpd stop\`
+2. \`Shutdown\` the \`database\` services on db server nodes - \`service mysql stop\`
+3. \`Restart\` \`all\` servers (web and db) at once - \`/sbin/shutdown -r\`
+4. \`Start\` the \`database\` services on db server nodes - \`service mysql start\`
+5. \`Start\` the \`web\` services on web server nodes - \`service httpd start\`
+
+> Внимание: Не используй этот \`playbook\` в реальной установке. Есть лучшие варианты реализвации этих действий. Мы делаем это для закрепления основных навыков понимания \`playbooks\`.
+        `,
+        "subText": "",
+        "files" : [
+          {
+            "name": "simple-playbook.yml",
+            "stage": [{"name":"Stop the web services on web server nodes","hosts":"web_nodes","tasks":[{"name":"Stop the web services on web server nodes","command":"service httpd stop"}]}],
+            "answers": [[{"name":"Stop the web services on web server nodes","hosts":"web_nodes","tasks":[{"name":"Stop the web services on web server nodes","command":"service httpd stop"}]},{"name":"Shutdown the database services on db server nodes","hosts":"db_nodes","tasks":[{"name":"Shutdown the database services on db server nodes","command":"service mysql stop"}]},{"name":"Restart all servers (web and db) at once","hosts":"all_nodes","tasks":[{"name":"Restart all servers (web and db) at once","command":"/sbin/shutdown -r"}]},{"name":"Start the database services on db server nodes","hosts":"db_nodes","tasks":[{"name":"Start the database services on db server nodes","command":"service mysql start"}]},{"name":"Start the web services on web server nodes","hosts":"web_nodes","tasks":[{"name":"Start the web services on web server nodes","command":"service httpd start"}]}]]
+          },{
+            "name": "inventory",
+            "mode": "ini",
+            "readOnly": true,
+            "stage": `# Sample Inventory File
+
+# Web Servers
+sql_db1 ansible_host=sql01.xyz.com ansible_connection=ssh ansible_user=root ansible_ssh_pass=Lin$Pass
+sql_db2 ansible_host=sql02.xyz.com ansible_connection=ssh ansible_user=root ansible_ssh_pass=Lin$Pass
+web_node1 ansible_host=web01.xyz.com ansible_connection=ssh ansible_user=administrator ansible_ssh_pass=Win$Pass
+web_node2 ansible_host=web02.xyz.com ansible_connection=ssh ansible_user=administrator ansible_ssh_pass=Win$Pass
+web_node3 ansible_host=web03.xyz.com ansible_connection=ssh ansible_user=administrator ansible_ssh_pass=Win$Pass
+
+[db_nodes]
+sql_db1
+sql_db2
+
+[web_nodes]
+web_node1
+web_node2
+web_node3
+
+[all_nodes:children]
+db_nodes
+web_nodes`
+          }
+        ]
     }
   ]
