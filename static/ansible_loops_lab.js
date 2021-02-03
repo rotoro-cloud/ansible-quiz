c132f76e2b24fd94609553ac54fb246906c9b375
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2021-02-03 21:48:16 +0300

    Create ansible_loops_lab.js

---
+++ static/ansible_loops_lab.js
@@ -0,0 +1,113 @@
+var questions = [
+    {
+        "title": "Ansible Loops",
+        "question": "В данный момент playbook запускает команду, которая выводит на экран название фрукта. Примени wbrk (директиву `with_items`) в этом task, чтобы отобразились все фрукты из списка преременной `fruits`.",
+        "subText": "",
+        "files" : [
+          {
+          "name": "sample-playbook.yml",
+          "stage": [{
+  "name": "Print list of fruits",
+  "hosts": "localhost",
+  "vars": {
+    "fruits": [
+      "Apple",
+      "Banana",
+      "Grapes",
+      "Orange"
+    ]
+  },
+  "tasks": [
+    {
+      "command": "echo \"Apple\""
+    }
+  ]
+}],
+          "answers": [[
+  {
+    "name": "Print list of fruits",
+    "hosts": "localhost",
+    "vars": {
+      "fruits": [
+        "Apple",
+        "Banana",
+        "Grapes",
+        "Orange"
+      ]
+    },
+    "tasks": [
+      {
+        "command": "echo \"{{ item }}\"",
+        "with_items": "{{ fruits }}"
+      }
+    ]
+  }
+]]
+          }
+        ]
+    },
+    {
+        "title": "Ansible Loops",
+        "question": "Более реалистичный вариант использования. Нам нужно установить несколько пакетов с помощью модуля `yum`. Текущий playbook устанавливает installs только отдельный пакет.",
+        "subText": "",
+        "files" : [
+          {
+            "name": "sample-playbook.yml",
+            "stage": [{
+  "name": "Install required packages",
+  "hosts": "localhost",
+  "vars": {
+    "packages": [
+      "httpd",
+      "binutils",
+      "glibc",
+      "ksh",
+      "libaio",
+      "libXext",
+      "gcc",
+      "make",
+      "sysstat",
+      "unixODBC",
+      "mongodb",
+      "nodejs",
+      "grunt"
+    ]
+  },
+  "tasks": [
+    {
+      "yum": "name=httpd state=present"
+    }
+  ]
+}],
+            "answers": [[{"name":"Install required packages","hosts":"localhost","vars":{"packages":["httpd","binutils","glibc","ksh","libaio","libXext","gcc","make","sysstat","unixODBC","mongodb","nodejs","grunt"]},"tasks":[{"yum":{"name":"{{ item }}","state":"present"},"with_items":"{{ packages }}"}]}],
+                       [{
+  "name": "Install required packages",
+  "hosts": "localhost",
+  "vars": {
+    "packages": [
+      "httpd",
+      "binutils",
+      "glibc",
+      "ksh",
+      "libaio",
+      "libXext",
+      "gcc",
+      "make",
+      "sysstat",
+      "unixODBC",
+      "mongodb",
+      "nodejs",
+      "grunt"
+    ]
+  },
+  "tasks": [
+    {
+      "yum": "name={{ item }} state=present",
+      "with_items": "{{ packages }}"
+    }
+  ]
+}]]
+          }
+        ]
+    }
+  ]
